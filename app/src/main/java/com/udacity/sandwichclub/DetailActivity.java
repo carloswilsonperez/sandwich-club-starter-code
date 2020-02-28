8fd36053ea675a512fb010e18d3b40a72ff279f9
Carlos Wilson <carlos.wilson@propelics.com>
2020-02-28 17:16:53 -0600

    Solution

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -37,6 +41,46 @@ public class DetailActivity extends AppCompatActivity {
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
+        // Show the content for the name property
+        TextView name = findViewById(R.id.name);
+        name.setText(sandwich.getMainName());
+
+        // Show the content for the alsoKnownAs property
+        TextView alsoKnownAs = findViewById(R.id.known_as);
+        List<String> alsoKnownList = sandwich.getAlsoKnownAs();
+        String alsoKnownString = "";
+        for (int i = 0; i < alsoKnownList.size(); i++) {
+            alsoKnownString += alsoKnownList.get(i) ;
+            if (i < alsoKnownList.size() - 1) {
+                alsoKnownString += "\n";
+            }
+        }
+
+        alsoKnownAs.setText(alsoKnownString);
+
+        // Show the content for the placeOfOrigin property
+        TextView placeOfOrigin = findViewById(R.id.place_of_origin);
+        placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
+
+        // Show the content for the description property
+        TextView description = findViewById(R.id.description_tv);
+        description.setText(sandwich.getDescription());
+
+        // Show the content for the ingredients property
+        TextView ingredients = findViewById(R.id.ingredients_text);
+        List<String> ingredientsList = sandwich.getIngredients();
+        String ingredientsString = "";
+        for (int i = 0; i < ingredientsList.size(); i++) {
+            ingredientsString += ingredientsList.get(i);
+            if (i < ingredientsList.size() - 1) {
+                ingredientsString += "\n";
+            }
+        }
+
+        ingredients.setText(ingredientsString);
+
+
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -44,16 +88,14 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
+
+        Picasso.with(this).load(sandwich.getImage()).into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
         finish();
-        Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
     private void populateUI() {
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,72 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+        JSONObject sandwhich;
+        JSONObject name;
+        String mainName;
+        JSONArray alsoKnownAs;
+        List<String> alsoKnownAsList;
+        String placeOfOrigin;
+        String description;
+        String image;
+        JSONArray ingredients;
+        List<String> ingredientsList;
+        Sandwich sandWhichSelected;
+
+        try {
+            // Initialize the JSONObject instance
+            sandwhich = new JSONObject(json);
+
+            // Extract the name property
+            name = sandwhich.getJSONObject("name");
+
+            // Extract mainName property
+            mainName = name.getString("mainName");
+
+            // Extract alsoKnownAs property
+            alsoKnownAs = name.getJSONArray("alsoKnownAs");
+            alsoKnownAsList = new ArrayList<String>();
+            for (int i = 0; i < alsoKnownAs.length(); i++) {
+                alsoKnownAsList.add(alsoKnownAs.getString(i));
+            }
+
+            // Extract the placeOfOrigin property
+            placeOfOrigin = sandwhich.getString("placeOfOrigin");
+
+            // Extract the description property
+            description = sandwhich.getString("description");
+
+            // Extract the image property
+            image = sandwhich.getString("image");
+
+            // Extract the ingredients property
+            ingredients = sandwhich.getJSONArray("ingredients");
+            ingredientsList = new ArrayList<String>();
+            for (int i = 0; i < ingredients.length(); i++) {
+                ingredientsList.add(ingredients.getString(i));
+            }
+
+            // Create sandwhich instance
+            sandWhichSelected = new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredientsList);
+            return sandWhichSelected;
+
+        } catch (JSONException e) {
+            Log.e("MYAPP", "unexpected JSON exception", e);
+        }
+
         return null;
     }
 }
